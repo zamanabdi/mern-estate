993194f2ae3567c2bf42d8ad2d6211222ddf9448
zamanabdi <zaman.abdi24@gmail.com>
2024-03-25 14:35:04 +0530

    changes made in package.json

--- api/index.js
+++ api/index.js
@@ -3,5 +3,5 @@ import express from "express"
 const app = express();
 
 app.listen(3000, () => {
-    console.log("server is running on port 3000!!! .....hellooo world");
+    console.log("server running on port 3000...");
 })
