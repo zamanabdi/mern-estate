6191db320658a6979862d402c65bbcbdc2230d4a
zamanabdi <zaman.abdi24@gmail.com>
2024-03-31 20:40:31 +0530

    create a middleware and a function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async(req,res) => {
+export const signup = async(req,res,next) => {
 
     const {username,email,password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
@@ -12,7 +13,7 @@ export const signup = async(req,res) => {
     res.status(201).json('User created successfully');
         
     } catch (err) {
-        res.status(500).json(err.message);
+        next(err);
         
     }
     
--- api/index.js
+++ api/index.js
@@ -6,18 +6,33 @@ import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log("Connected to mongoDB database...")
-}).catch((err) => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to mongoDB database...");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("server running on port 3000...");
-})
+  console.log("server running on port 3000...");
+});
 
-app.use('/api/user', userRouter);
+app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+//creating a middleware to handle errors:-
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
